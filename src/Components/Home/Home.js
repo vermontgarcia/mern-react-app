c31b003854ed7b4f1cb42486d3c94329c39c1b2e
Vermont <vermont.ironhack@gmail.com>
2018-12-08 14:25:54 -0600

    Basic Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,9 +16,16 @@ class Home extends Component {
     console.log(this.props);
     return (
       <div>
+
+        {/*
         <h1>Home {this.props.state ? this.props.state.user.username : null}</h1>
         <strong>{this.props.state.user.loggedIn ? this.props.state.user.email: "Usuario no logged"}</strong>
                 <button onClick={this.props.handleLogin}>Login</button>
+        */}
+
+        <h1>Home</h1>
+
+
       </div>
     )
   }
