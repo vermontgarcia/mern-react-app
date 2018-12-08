8c779acc4c523dee8a8ec57812939ab9c2a36c8a
Vermont <vermont.ironhack@gmail.com>
2018-12-08 14:21:18 -0600

    Routing with global state and handles

--- src/Router.js
+++ src/Router.js
@@ -2,18 +2,46 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Components/Home/Home'
 import Login from './Components/Login/Login';
-import SignUp from './Components/Signup/Signup';
+import Signup from './Components/Signup/Signup';
 import Profile from './Components/Profile/Profile';
 import NotFound from './404';
 
 
 
-const Router = ({state, handleLogin}) => (
+const Router = ({state, handleLogin, handleChange}) => (
   <Switch>
-    <Route exact path='/' render={(props) => <Home handleLogin={handleLogin} state={state} {...props} />} />
-    <Route exact path='/login' component={Login} />
-    <Route exact path='/signup' component={SignUp} />
-    <Route exact path='/profile' component={Profile} />
+    <Route exact path='/' render={(props) => (
+      <Home 
+        handleLogin={handleLogin}
+        handleChange={handleChange}
+        state={state}
+        {...props}
+        />)}
+        />
+    <Route exact path='/login' render={(props) => (
+      <Login
+        handleLogin={handleLogin}
+        handleChange={handleChange}
+        state={state}
+        {...props}
+      />)}
+    />
+    <Route exact path='/signup' render={(props) => (
+      <Signup
+        //handleLogin={handleLogin}
+        handleChange={handleChange}
+        state={state}
+        {...props}
+      />)}
+    />
+    <Route exact path='/profile' render={(props) => (
+      <Profile
+        //handleLogin={handleLogin}
+        //handleChange={handleChange}
+        state={state}
+        {...props}
+      />)}
+    />
     <Route path='*' component={NotFound} />
   </Switch>
 )
