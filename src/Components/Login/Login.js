5a8bb8eda899ea0c9020eef49cdeda678e50b60a
Vermont <vermont.ironhack@gmail.com>
2018-12-08 14:26:33 -0600

    Login component working

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,22 +1,28 @@
 import React, {Component} from 'react';
+import InputField from '../Common/InputField';
+import FormButton from '../Common/FromButton';
 
 class Login extends Component {
-  constructor(){
-    super();
-    this.state = {
-
-    }
-  }
+  //constructor(){
+  //  super();
+  //  this.state = {
+  //  }
+  //}
 
   componentWillMount (){
 
   }
 
   render(){
-    console.log(this.props);
+    //console.log(this.props);
     return (
       <div>
         <h1>Login</h1>
+        <form onSubmit={this.props.handleLogin}>
+        <InputField title='Email:' type='email' name='email' handleChange={this.props.handleChange} />
+        <InputField title='Password:' type='password' name='password' handleChange={this.props.handleChange} />
+        <FormButton className='form-button button' type='submit' name='Log in' />
+        </form>
       </div>
     )
   }
