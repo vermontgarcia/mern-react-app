854d5e93ce559da38229ae6b1daf880d4e662ea8
Vermont <vermont.ironhack@gmail.com>
2018-12-10 16:45:27 -0600

    Placeholder and Autofocus implemented

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import InputField from '../Common/InputField';
 import FormButton from '../Common/FromButton';
+import {Link} from 'react-router-dom';
 
 class Login extends Component {
   //constructor(){
@@ -19,10 +20,13 @@ class Login extends Component {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.props.handleLogin}>
-        <InputField title='Email:' type='email' name='email' handleChange={this.props.handleChange} />
-        <InputField title='Password:' type='password' name='password' handleChange={this.props.handleChange} />
-        <FormButton className='form-button button' type='submit' name='Log in' />
+          <InputField title='Email:' type='email' name='email' placeholder='user@copareit.com' handleChange={this.props.handleChange} autofocus='autofocus' />
+          <InputField title='Password:' type='password' name='password' placeholder='Your password' handleChange={this.props.handleChange} />
+          <FormButton className='form-button button' type='submit' name='Log in' />
         </form>
+        <div>
+          <p>Don't have an account yet? <span><Link to='/signup' >Register</Link></span></p>
+        </div>
       </div>
     )
   }
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import InputField from '../Common/InputField';
 import FormButton from '../Common/FromButton';
+import {Link} from 'react-router-dom';
 
 class Signup extends Component {
   render(){
@@ -8,12 +9,15 @@ class Signup extends Component {
       <div>
         <h1>Signup</h1>
         <form onSubmit={this.props.handleSignup}>
-          <InputField title='Name:' type='text' name='name' handleChange={this.props.handleChange} />
-          <InputField title='Email:' type='email' name='email' handleChange={this.props.handleChange} />
-          <InputField title='Password:' type='password' name='password' handleChange={this.props.handleChange} />
-          <InputField title='Confirm Password:' type='password' name='confirmPassword' handleChange={this.props.handleChange} />
+          <InputField title='Name:' type='text' name='name' placeholder='What is your name?' handleChange={this.props.handleChange} autofocus='autofocus' />
+          <InputField title='Email:' type='email' name='email' placeholder='user@copareit.com' handleChange={this.props.handleChange} />
+          <InputField title='Password:' type='password' name='password' placeholder='Your password' handleChange={this.props.handleChange} />
+          <InputField title='Confirm Password:' type='password' name='confirmPassword' placeholder='Same password' handleChange={this.props.handleChange} />
           <FormButton className='form-button button' type='submit' name='Sign up' />
         </form>
+        <div>
+          <p>Already have an account? <span><Link to='/login' >Log in</Link></span></p>
+        </div>
       </div>
     )
   }
