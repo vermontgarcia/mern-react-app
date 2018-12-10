85997544c3bdbed632456902d104035fd3d99595
Vermont <vermont.ironhack@gmail.com>
2018-12-10 09:20:01 -0600

    IsLoggedIn checker added and logout option added

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react'
+import {isLoggedIn} from '../../authService'
 
 class Profile extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentWillMount (){
+
+    const token = localStorage.getItem('token');
+    token ? isLoggedIn(this.props.history) : this.props.history.push('/login');
+
+  }
+
+
   render(){
     return(
-      <h1>Profile</h1>
+      <div>
+        <h1>Profile</h1>
+        <p onClick={this.props.handleLogout}>Logout</p>
+      </div>
     )
   }
 }
