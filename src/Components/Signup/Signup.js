b4367affa46aae5499d042484755ef9d9fb415d8
Vermont <vermont.ironhack@gmail.com>
2018-12-10 09:21:12 -0600

    Signup form implemented

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react'
+import InputField from '../Common/InputField';
+import FormButton from '../Common/FromButton';
 
 class Signup extends Component {
   render(){
     return(
-      <h1>Signup</h1>
+      <div>
+        <h1>Signup</h1>
+        <form onSubmit={this.props.handleSignup}>
+          <InputField title='Name:' type='text' name='name' handleChange={this.props.handleChange} />
+          <InputField title='Email:' type='email' name='email' handleChange={this.props.handleChange} />
+          <InputField title='Password:' type='password' name='password' handleChange={this.props.handleChange} />
+          <InputField title='Confirm Password:' type='password' name='confirmPassword' handleChange={this.props.handleChange} />
+          <FormButton className='form-button button' type='submit' name='Sign up' />
+        </form>
+      </div>
     )
   }
 }
