34b515a732900e654c517541820f22be71ee1b1a
Vermont <vermont.ironhack@gmail.com>
2018-12-10 09:25:57 -0600

    Profile defined as home in router

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import Home from './Components/Home/Home'
+//import Home from './Components/Home/Home'
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
 import Profile from './Components/Profile/Profile';
@@ -8,16 +8,15 @@ import NotFound from './404';
 
 
 
-const Router = ({state, handleLogin, handleChange}) => (
+const Router = ({state, handleLogin, handleLogout, handleChange, handleSignup}) => (
   <Switch>
     <Route exact path='/' render={(props) => (
-      <Home 
-        handleLogin={handleLogin}
-        handleChange={handleChange}
+      <Profile
+        handleLogout={handleLogout}
         state={state}
         {...props}
-        />)}
-        />
+      />)}
+    />
     <Route exact path='/login' render={(props) => (
       <Login
         handleLogin={handleLogin}
@@ -28,20 +27,13 @@ const Router = ({state, handleLogin, handleChange}) => (
     />
     <Route exact path='/signup' render={(props) => (
       <Signup
-        //handleLogin={handleLogin}
+        handleSignup={handleSignup}
         handleChange={handleChange}
         state={state}
         {...props}
       />)}
     />
-    <Route exact path='/profile' render={(props) => (
-      <Profile
-        //handleLogin={handleLogin}
-        //handleChange={handleChange}
-        state={state}
-        {...props}
-      />)}
-    />
+    
     <Route path='*' component={NotFound} />
   </Switch>
 )
