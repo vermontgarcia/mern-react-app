10b9edec40640dff08bde13f8546dfbbf6ba483c
Vermont <vermont.ironhack@gmail.com>
2018-12-10 16:46:21 -0600

    Profile user data

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,28 +1,56 @@
-import React, {Component} from 'react'
-import {isLoggedIn} from '../../authService'
+import React, {Component} from 'react';
+import {isLoggedIn} from '../../authService';
+import ProfileField from './ProfileField';
 
 class Profile extends Component {
 
   constructor(){
     super();
     this.state = {
-
+      user: {}
     }
   }
 
-  componentWillMount (){
+  componentWillMount(){
 
     const token = localStorage.getItem('token');
-    token ? isLoggedIn(this.props.history) : this.props.history.push('/login');
+
+    token ? isLoggedIn(this.props.history) : this.props.history.push('/login')
+
+
+    /*
+    if (token){
+      isLoggedIn(this.props.history)
+      const user = JSON.parse(localStorage.getItem('user'))
+      this.setState({user}) 
+    } else {
+      this.props.history.push('/login');
+    } 
+    */
 
   }
 
 
   render(){
+    console.log(this.props)
+    const {user} = this.props.state
     return(
       <div>
-        <h1>Profile</h1>
-        <p onClick={this.props.handleLogout}>Logout</p>
+        <div>
+          <h1>Profile</h1>
+        </div>
+
+        <div>
+          <img className='profile-picture' src={user.profilePicture === 'avatar' ? '/avatar.png' : user.profilePicture} alt={user.name} />
+          <ProfileField title='Name' name={user.name} />
+          <ProfileField title='Email' name={user.email} />
+          <ProfileField title='Role' name={user.role} />
+          <ProfileField title='Status' name={user.status} />
+        </div>
+
+        <div>
+          <p onClick={this.props.handleLogout}>Logout</p>
+        </div>
       </div>
     )
   }
