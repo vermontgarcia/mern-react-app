659336f1ea1e9c8eacbe8126b2cf8f3f97964fbe
Vermont <vermont.ironhack@gmail.com>
2018-12-10 16:42:54 -0600

    Placeholder and autofocus implemented

--- src/Components/Common/InputField.js
+++ src/Components/Common/InputField.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const InputField = ({handleChange, title, type, name}) => (
-  <div className='form-field'>
+const InputField = ({handleChange, title, type, name, placeholder, autofocus}) => (
+  <div className='form-field-envelop'>
     <label className='label'>
       {title}
       <input className='input'
         type={type}
         name={name}
+        placeholder={placeholder}
+        autoFocus={autofocus}
         onChange={handleChange} />     
     </label>
   </div>
