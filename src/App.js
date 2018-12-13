f6996685514623c50774c37d4207e9273e99b52a
Vermont <vermont.ironhack@gmail.com>
2018-12-12 19:42:18 -0600

    HandleSetState added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {withRouter} from 'react-router-dom';
 import {login} from './authService';
 import {logout} from './authService';
 import {signup} from './authService';
+import Nav from './Components/Nav/Nav';
 
 class App extends Component {
 
@@ -38,28 +39,33 @@ class App extends Component {
     let field = e.target.name;
     user[field] = e.target.files ? e.target.files[0] : e.target.value;
     this.setState({user})
-    //console.log(this.state.user);
   }
 
   handleRedirect = () => {
     this.props.history.push('/login');
   }
 
+  handleSetState = () => {
+    const user = JSON.parse(localStorage.getItem('user'))
+    user ? this.setState({user}) : this.props.history.push('/login');
+  }
+
   componentWillMount(){
     const user = JSON.parse(localStorage.getItem('user'))
     user ? this.setState({user}) : this.props.history.push('/login');
+    //user != null || user != undefined ? this.setState({user}) : this.props.history.push('/login');
   }
 
 
 
   render() {
-    //console.log("appjs", this.props);
     return (
       <div className="App">
         {/*
         <nav>
             <button onClick={this.handleRedirect}>{this.state.user.loggedIn ? this.state.user.username: "login"}</button>
         </nav>
+        <Nav />
         */}
         <Router 
           state={this.state}
@@ -67,6 +73,7 @@ class App extends Component {
           handleLogout={this.handleLogout}
           handleSignup={this.handleSignup}
           handleChange={this.handleChange}
+          handleSetState={this.handleSetState}
         />
       </div>
     );
