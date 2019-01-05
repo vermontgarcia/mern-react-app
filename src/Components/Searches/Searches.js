eb76a7d86f0d4ced110cd7ee09ec66ebf65b9575
Vermont <vermont.ironhack@gmail.com>
2019-01-05 07:37:56 -0600

    Searches log

--- src/Components/Searches/Searches.js
+++ src/Components/Searches/Searches.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {isLoggedIn} from '../../authService';
 import {getMySearches} from '../../service';
+import {addProduct} from '../../service';
 import Nav from '../Nav/Nav';
 import FooterData from '../Common/FooterData';
+import Item from '../Search/Item';
 
 import {Layout} from 'antd';
 import {Icon} from 'antd';
 import {BackTop} from 'antd';
+import {Collapse} from 'antd';
+import {message} from 'antd';
 
-const {Header, Footer, Content} = Layout
+const {Header, Footer, Content} = Layout;
+const Panel = Collapse.Panel;
 
 class Searches extends Component {
   constructor(){
@@ -16,6 +21,16 @@ class Searches extends Component {
     this.state = {
       searches: []
     }
+  }
+
+  handleAddList = (item, e) => {
+    //console.log('Adding to list....', item)
+    item.userId = this.props.state.user._id
+    addProduct(item)
+      .then(res => {
+        //console.log('Add Item =====>', res.data.msg)
+        message.success(res.data.msg);
+      })
   }
 
   componentWillMount(){
@@ -41,6 +56,7 @@ class Searches extends Component {
 
   render() {
     const {user} = this.props.state;
+    const {searches} = this.state;
     return (
       <div>
         <Layout>
@@ -48,8 +64,19 @@ class Searches extends Component {
             <Nav user={user} handleLogout={this.props.handleLogout} />
           </Header>
           <Content>
-            <div className='profile-data'>
+            <div className='searches-envelop'>
             <h1>Mis Búsquedas</h1>
+
+            <Collapse accordion>
+              {
+                searches.map((search, index) => (
+                  <Panel header={`Busqueda por "${search.search}" @ el "${search.created_at}"`} key={index}>
+                    {search.items.map((item, index) => <Item key={index} item={item}  handleAddList={this.handleAddList} /> )} 
+                  </Panel>
+                ))
+              } 
+
+            </Collapse>
             
         </div>
           </Content>
