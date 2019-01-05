5b6fa4be5abcb69ac61ede384aa9103274c72f99
vermontgarcia <vermont.ironhack@gmail.com>
2019-01-05 00:09:24 -0600

    Table added

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -3,11 +3,18 @@ import {isLoggedIn} from '../../authService';
 import {getMyList} from '../../service';
 import Nav from '../Nav/Nav';
 
-import {Layout} from 'antd';
-import {Icon} from 'antd';
-import {BackTop} from 'antd';
+import {
+  Layout,
+  Icon,
+  BackTop,
+  Table
+} from 'antd';
 
 const {Header, Footer, Content} = Layout
+
+function onChange(pagination, filters, sorter) {
+  console.log('params', pagination, filters, sorter);
+}
 
 class List extends Component {
   constructor(){
@@ -30,8 +37,16 @@ class List extends Component {
       .then(res => {
         console.log('List Data =====>', res.data.msg)
         let list = res.data.list;
-        this.setState({list})
-        console.log('List from state =====>', this.state.list)
+        let walmart = []
+        let superama = []
+
+        list.forEach(item => {
+          if(item.market === 'Walmart') walmart.push(item);
+          if(item.market === 'Superama') superama.push(item);        
+        });
+              
+        this.setState({list, walmart, superama})
+        console.log('List from state =====>', this.state)
       })
       .catch((err) => {
         console.log('Get List Error =====> ', err.response);
@@ -39,8 +54,35 @@ class List extends Component {
       });
   }
 
+  
+
   render() {
     const {user} = this.props.state;
+
+    const columns = [{
+      title: 'Super',
+      dataIndex: 'market',
+      filters: [{
+        text: 'Superama',
+        value: 'Superama',
+      }, {
+        text: 'Walmart',
+        value: 'Walmart',
+      }],
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      onFilter: (value, record) => record.market.indexOf(value) === 0,
+      //sorter: (a, b) => a.name.length - b.name.length,
+    }, {
+      title: 'Descripcion',
+      dataIndex: 'name',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.name - b.name,
+    }, {
+      title: 'Precio',
+      dataIndex: 'price'
+    }];
+
     return (
       <div>
         <Layout>
@@ -50,6 +92,8 @@ class List extends Component {
           <Content>
             <div className='profile-data'>
             <h1>Mi lista</h1>
+
+            <Table columns={columns} dataSource={this.state.list} onChange={onChange} />
             
         </div>
           </Content>
